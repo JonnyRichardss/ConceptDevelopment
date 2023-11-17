cf50129276834644163ef642af93cf2fc03da31a
Jonny <26541501@students.lincoln.ac.uk>
2023-11-17 14:58:16 +0000

    Added Resource holder with food and population

---
+++ Assets/ResourceHolder.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceHolder
+{
+    public float Food
+    {
+        get;
+        private set;
+    }
+    public float Population
+    {
+        get;
+        private set;
+    }
+
+}
