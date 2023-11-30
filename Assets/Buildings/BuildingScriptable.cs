12c399a2c1303b75097c17b606088864e0240bf9
Jonny <jonnyrichards254@gmail.com>
2023-11-30 12:48:53 +0000

    added building scriptable object

---
+++ Assets/Buildings/BuildingScriptable.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "My Assets/Buildings")]
+public class BuildingScriptable : ScriptableObject
+{
+    [SerializeField] string m_buildingName;
+    [SerializeField] string m_buildingText;
+    [SerializeField] Effect m_buildingEffect;
+}
+
