775758afa8db4d6b16577c6fcd06c8da96f39282
flubber914 <117917092+flubber914@users.noreply.github.com>
2024-01-08 02:05:15 +0000

    mip?GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,31 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
-
-using UnityEngine.UI;
-
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
     public static int currentSceneIndex;
     GameState state;
     public int TurnNumber;
-    ResourceHolder Resources; 
-
+    ResourceHolder Resources;
     public List<Effect> Effects;
     public List<BuildingScriptable> Buildings;
 
-    
-
-    GameState state;
-
-
-    public Text summaryText;
-
-
     Effect TurnSummary;
-    
+
     //awake is caleld even if the object is disabled apaprently
     private void Awake()
     {
@@ -62,70 +46,11 @@ public class GameManager : MonoBehaviour
     #region GameLoop
     public void NewTurn()
     {
-        if(TurnNumber != 0)
+        if (TurnNumber != 0)
         {
             AddBuildings(); //add building effects to effects list
             ApplyAllEffects(); //apply
-            DisplaySummary();//display summary here
-        }
-        TurnNumber++;
-        Debug.Log("Resource Showing not Implemented yet");
-
-        //AdvanceState();
-        //TryNextEvent();
-        //turn logging goes in here
-    }
-    private void DisplaySummary()
-    {
-        //Canvas with a Text UI element named "SummaryText"
-        if (summaryText != null)
-        {
-            // Customize this message based on the summary you want to display
-            string summaryMessage = $"Turn {TurnNumber} summary:\nBuildings added, effects applied.";
-
-            // Set the text of the UI element
-            summaryText.text = summaryMessage;
-
-            // Activate the Canvas or show the pop-up
-            summaryText.gameObject.SetActive(true);
-
-            //Coroutine or a timer to hide the pop-up after some time
-            StartCoroutine(HideSummaryAfterDelay(3f)); // Hide after 3 seconds (adjust as needed)
-        }
-    }
-
-    private IEnumerator HideSummaryAfterDelay(float delay)
-    {
-        yield return new WaitForSeconds(delay);
-
-        //Canvas with a Text UI element named "SummaryText"
-        if (summaryText != null)
-        {
-            //Hide the pop-up
-            summaryText.gameObject.SetActive(false);
-        }
-    }
-    public void DrawEvents()
-    {
-        for (int RemainingEvents=4;RemainingEvents == 0; RemainingEvents--)
-        {
-            EventManager.DrawEvent(Resources);
-            //do more here
-            //probably ApplyChoice()
-        }
-        AdvanceState();
-        Debug.Log("Event Drawing not implemented yet");
-    }
-
-    #endregion
-    #region GameLoop
-    public void NewTurn()
-    {
-        if(TurnNumber != 0)
-        {
-            AddBuildings(); //add building effects to effects list
-            ApplyAllEffects(); //apply
-            DisplaySummary(); //display summary here
+            //display summary here
         }
         TurnNumber++;
         Debug.Log("Resource Showing not Implemented yet");
@@ -135,40 +60,9 @@ public class GameManager : MonoBehaviour
         //turn logging goes in here
     }
 
-    private void DisplaySummary()
-    {
-        //Canvas with a Text UI element named "SummaryText"
-        if (summaryText != null)
-        {
-            // Customize this message based on the summary you want to display
-            string summaryMessage = $"Turn {TurnNumber} summary:\nBuildings added, effects applied.";
-
-            // Set the text of the UI element
-            summaryText.text = summaryMessage;
-
-            // Activate the Canvas or show the pop-up
-            summaryText.gameObject.SetActive(true);
-
-            //Coroutine or a timer to hide the pop-up after some time
-            StartCoroutine(HideSummaryAfterDelay(3f)); // Hide after 3 seconds (adjust as needed)
-        }
-    }
-
-    private IEnumerator HideSummaryAfterDelay(float delay)
-    {
-        yield return new WaitForSeconds(delay);
-
-        //Canvas with a Text UI element named "SummaryText"
-        if (summaryText != null)
-        {
-            //Hide the pop-up
-            summaryText.gameObject.SetActive(false);
-        }
-    }
-
     public void DrawEvents()
     {
-        for (int RemainingEvents=4;RemainingEvents == 0; RemainingEvents--)
+        for (int RemainingEvents = 4; RemainingEvents == 0; RemainingEvents--)
         {
             EventManager.DrawEvent(Resources);
             //do more here
@@ -177,7 +71,6 @@ public class GameManager : MonoBehaviour
         AdvanceState();
         Debug.Log("Event Drawing not implemented yet");
     }
-
     public void DrawBuildings()
     {
         List<BuildingScriptable> buildings = BuildingManager.DrawBuildings(Resources);
@@ -188,11 +81,10 @@ public class GameManager : MonoBehaviour
     }
     #endregion
     #region ApplyEffects
-
     private void ApplyAllEffects()
     {
         List<Effect> effectsTemp = new List<Effect>();
-        foreach(Effect effect in Effects)
+        foreach (Effect effect in Effects)
         {
             if (Resources.TryApplyEffect(effect))
             {
@@ -204,7 +96,7 @@ public class GameManager : MonoBehaviour
     }
     public void AddBuildings()
     {
-        foreach(BuildingScriptable b in Buildings)
+        foreach (BuildingScriptable b in Buildings)
         {
             Effects.Add(b.m_buildingEffect);
         }
