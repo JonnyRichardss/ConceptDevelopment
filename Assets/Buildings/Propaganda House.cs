7194b25d69f15f04b52bd08dbdff15331eaf67a8
JayUoL <25691612@students.lincoln.ac.uk>
2024-01-15 13:29:36 +0000

    update of scripts

--- Assets/Buildings/Propaganda House.cs
+++ Assets/Buildings/Propaganda House.cs
@@ -6,6 +6,7 @@ public class PropagandaHouse : MonoBehaviour
 {
     BuildingController building;
     public bool isPlaced;
+    private bool isCheckingPlacement = true; // Boolean to control the continuous checking
 
     // Start is called before the first frame update
     void Start()
@@ -13,20 +14,23 @@ public class PropagandaHouse : MonoBehaviour
         building = transform.parent.GetComponent<BuildingController>();
     }
 
-
-    private void OnTriggerEnter(Collider other)
+    // FixedUpdate is called every fixed frame-rate frame
+    void FixedUpdate()
     {
-        if (other.gameObject.tag == "HousePlacementArea")
+        // Check if the building is not placed and we are still allowed to check placement
+        if (!isPlaced && isCheckingPlacement)
         {
-            BuildingPlacementArea placementArea = other.GetComponent<BuildingPlacementArea>();
-            if (placementArea != null && placementArea.CanPlaceBuilding())
-            {
-                PlacePropagandaHouse();
-            }
-            else
-            {
-                Debug.Log("Cannot place Propaganda House here.");
-            }
+            CheckBuildingPlacement();
+        }
+    }
+
+    private void CheckBuildingPlacement()
+    {
+        // Check if the associated BuildingController is placed
+        if (building.isPlaced)
+        {
+            PlacePropagandaHouse();
+            isCheckingPlacement = false; // Stop continuous checking once placed
         }
     }
 
@@ -37,7 +41,6 @@ public class PropagandaHouse : MonoBehaviour
             building.isPlaced = true;
             isPlaced = true;
 
-            
             Effect propagandaEffect = new Effect(0, 0, 0, 1, 0, 0);
             GameManager.instance.Effects.Add(propagandaEffect);
             GameManager.instance.ApplyChoiceChange(propagandaEffect);
@@ -48,7 +51,5 @@ public class PropagandaHouse : MonoBehaviour
         {
             Debug.Log("Propaganda House has already been placed.");
         }
-    }   
-
-
+    }
 }
